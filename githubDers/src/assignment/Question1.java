6e5701b0376ca98bdbc75f6aea4dc5499f190775
a <a@DESKTOP-6FC4VPA.GAST>
2022-12-08 21:14:48 +0300

    first assignment commit

---
+++ githubDers/src/assignment/Question1.java
@@ -0,0 +1,77 @@
+package assignment;
+
+public class Question1 {
+	
+	public static void main(String[] args) {
+	
+	boolean isNice= false;
+	boolean isFast= true;
+	boolean isGood= true;
+	
+	System.out.println(isNice);
+	System.out.println(isFast);
+	System.out.println(isGood);
+	
+	char lastLetter='z';
+	char favoriteNumber='5';
+	char firstNumber='0';
+	
+	System.out.println(lastLetter);
+	System.out.println(favoriteNumber);
+	System.out.println(firstNumber);
+	
+	byte schoolNo = 38;
+	byte homeNo = 7;
+	byte streetNo = 5;
+	
+	System.out.println(schoolNo);
+	System.out.println(homeNo);
+	System.out.println(streetNo);
+	
+	short totalStudents = 2555;
+	short totalAmount = 1250;
+	short questionNumbers = 1589;
+	
+	System.out.println(totalStudents);
+	System.out.println(totalAmount);
+	System.out.println(questionNumbers);
+	
+	int bugsNumber= 25420;
+	int animalsNumber=57896;
+	int zonguldakPopulation= 290548;
+	
+	System.out.println(bugsNumber);
+	System.out.println(animalsNumber);
+	System.out.println(zonguldakPopulation);
+	
+	long totalEuro= 200000l;
+	long totalAnts= 2548795l;
+	long carsNumber= 6548794l;
+	
+	System.out.println(totalEuro);
+	System.out.println(totalAnts);
+	System.out.println(carsNumber);
+	
+	float givenNumbers= 7894561132f;
+	float askedNumbers= 963852741f;
+	float positiveNumbers=45678964f;
+	
+	System.out.println(givenNumbers);
+	System.out.println(askedNumbers);
+	System.out.println(positiveNumbers);
+	
+	double average=10.5;
+	double percentage=33.3;
+	double probability=66.6;
+	
+	System.out.println(average);
+	System.out.println(percentage);
+	System.out.println(probability);
+	
+	
+	
+	
+	
+	
+	}
+}
---
+++ githubDers/src/assignment/Question2.java
@@ -0,0 +1,35 @@
+package assignment;
+
+public class Question2 {
+	
+	public static void main(String[] args) {
+	
+	System.out.println("byte max value= " +Byte.MAX_VALUE);
+	System.out.println("short min value= " +Byte.MIN_VALUE);
+	
+	System.out.println("short max value= " +Short.MAX_VALUE);
+	System.out.println("short min value= " +Short.MIN_VALUE);
+	
+	System.out.println("int max value= " +Integer.MAX_VALUE);
+	System.out.println("int min value= " +Integer.MIN_VALUE);
+	
+	System.out.println("long max value= " +Long.MAX_VALUE);
+	System.out.println("long min value= " +Long.MIN_VALUE);
+	
+	
+	System.out.println("float max value= " +Float.MAX_VALUE);
+	System.out.println("float min value= " +Float.MIN_VALUE);
+	
+	
+	System.out.println("double max value= " +Double.MAX_VALUE);
+	System.out.println("double min value= " +Double.MIN_VALUE);
+	
+	
+	
+	
+	
+	
+	
+
+}
+}
